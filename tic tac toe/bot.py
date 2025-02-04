e3ce1fd61af033bf279ab24ea3e134939dba129b
cherryBepis <119437132+cherryBepis@users.noreply.github.com>
2025-02-04 19:14:01 +0300

    Update bot.py
    add comments

--- tic tac toe/bot.py
+++ tic tac toe/bot.py
@@ -2,25 +2,26 @@ import random
 from game_logic import check_win
 
 def get_bot_move(board):
-    """Логика хода бота."""
-    # Проверяем, можем ли мы выиграть
+    ''' bot follows three steps to decide its move:
+        1. check if it can win in the current move. if yes, make the winning move.
+        2. check if the player can win in the next move. if yes, block them.
+        3. if no immediate win or block is needed, pick a random available cell.
+    '''
     for i in range(3):
         for j in range(3):
             if board[i][j] == ' ':
                 board[i][j] = 'O'
                 if check_win(board, 'O'):
                     return (i, j)
-                board[i][j] = ' '  # Отменяем ход
+                board[i][j] = ' '  
 
-    # Проверяем, нужно ли блокировать игрока
     for i in range(3):
         for j in range(3):
             if board[i][j] == ' ':
                 board[i][j] = 'X'
                 if check_win(board, 'X'):
                     return (i, j)
-                board[i][j] = ' '  # Отменяем ход
+                board[i][j] = ' '  
 
-    # Если нет выигрыша или блокировки, выбираем случайную клетку
     available_moves = [(i, j) for i in range(3) for j in range(3) if board[i][j] == ' ']
     return random.choice(available_moves)
