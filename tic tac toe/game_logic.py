e41c35c36dcdce18a65d5244e6776ee3f00d8976
cherryBepis <119437132+cherryBepis@users.noreply.github.com>
2025-02-04 19:14:20 +0300

    Update game_logic.py
    add comments

--- tic tac toe/game_logic.py
+++ tic tac toe/game_logic.py
@@ -1,17 +1,14 @@
-
 def check_win(board, player):
-    """Проверка на победу игрока."""
     for i in range(3):
-        if all([cell == player for cell in board[i]]):  # Горизонталь
+        if all([cell == player for cell in board[i]]):  # horizontal
             return True
-        if all([board[j][i] == player for j in range(3)]):  # Вертикаль
+        if all([board[j][i] == player for j in range(3)]):  # vertical
             return True
-    if all([board[i][i] == player for i in range(3)]):  # Главная диагональ
+    if all([board[i][i] == player for i in range(3)]):  # main diag
         return True
-    if all([board[i][2 - i] == player for i in range(3)]):  # Побочная диагональ
+    if all([board[i][2 - i] == player for i in range(3)]):  # 2nd diag
         return True
     return False
 
 def check_draw(board):
-    """Проверка на ничью."""
     return all(cell != ' ' for row in board for cell in row)
